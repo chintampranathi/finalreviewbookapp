e1f1871016f4401089be127a974d6c02fc23518b
Pranathi <109022659+chintampranathi@users.noreply.github.com>
2026-04-26 22:59:01 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,10 @@ output "alb_dns_mumbai" {
   value       = module.alb_mumbai.alb_dns_name
 }
 
-output "alb_dns_hyderabad" {
-  description = "ALB DNS for Hyderabad region"
-  value       = module.alb_hyderabad.alb_dns_name
-}
+#output "alb_dns_hyderabad" {
+ # description = "ALB DNS for Hyderabad region"
+  #value       = module.alb_hyderabad.alb_dns_name
+#}
 
 output "rds_endpoint_mumbai" {
   description = "RDS primary endpoint (Mumbai)"
@@ -18,8 +18,8 @@ output "rds_endpoint_mumbai" {
   sensitive   = true
 }
 
-output "rds_endpoint_hyderabad" {
-  description = "RDS read replica endpoint (Hyderabad)"
-  value       = module.rds_hyderabad.db_endpoint
-  sensitive   = true
-}
+#output "rds_endpoint_hyderabad" {
+ # description = "RDS read replica endpoint (Hyderabad)"
+ # value       = module.rds_hyderabad.db_endpoint
+ # sensitive   = true
+#}
